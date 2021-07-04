206d3b60319b51e2394c04bdbdb2c832f7938f58
Jon Kartago Lamida <me@lamida.net>
2021-07-05 01:15:32 +0800

    Update LoA output

--- c/big/gen-all.sh
+++ c/big/gen-all.sh
@@ -1,8 +1,8 @@
 #echo "branches\nbranches2\nfactorial\nfib\nscarr\nnested-loop\nshadow\nsimple-linear\nsimple-loop\nuncallable" | xargs -I{} ./gen.sh {} $1
-echo "sqlite3" | xargs -I{} ./gen-scarr.sh {} $1
-echo "sqlite3" | xargs -I{} ./gen-scarr.sh {} no-opt
-echo "sqlite3" | xargs -I{} ./gen-loa.sh {} $1
-  echo "sqlite3" | xargs -I{} ./gen-loa.sh {} no-opt
+echo "althttpd\nbzip2" | xargs -I{} ./gen-scarr.sh {} $1
+echo "althttpd\nbzip2" | xargs -I{} ./gen-scarr.sh {} no-opt
+echo "althttpd\nbzip2" | xargs -I{} ./gen-loa.sh {} $1
+echo "althttpd\nbzip2" | xargs -I{} ./gen-loa.sh {} no-opt
 
 # Remove non main dot file
 find . -type f -name "*.dot" | grep -v main | xargs rm
