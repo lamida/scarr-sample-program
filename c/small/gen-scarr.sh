789a7a6f2d621616811dacc13c616878a8931e06
Jon Kartago Lamida <me@lamida.net>
2021-07-11 22:25:22 +0800

    Refactor script

--- c/small/gen-scarr.sh
+++ c/small/gen-scarr.sh
@@ -1,36 +1,80 @@
-if [ -z "$1" ]
-  then
-    echo "title is required"
-    exit 1
-fi
-
+#!/bin/bash
 title=$1
-if [ -n "$2" ] && [  "$2" = "no-opt" ]
-  then
-    echo "Running with no-opt"
-    out_title=$1-no-opt
-    clang -emit-llvm $title.c -S -Xclang -disable-O0-optnone -fno-discard-value-names -o $out_title.ll
-  else
-    out_title=$1
-    clang -emit-llvm $title.c -S -o $out_title.ll
+is_optimized=$2
+base_dir=$(pwd)
+
+if [ -z "$title" ]; then
+  echo "title is required"
+  exit 1
 fi
 
+LLVM_OPT_CMD=/home/lamida/github/llvm/llvm-project/llvm/cmake-build-release/bin/opt
+
+generate_dot() {
+  local dir_name_prefix=$1
+  local title=$2
+  local cfg_type=$3
+  local is_inline=$4
+  local is_optimized=$5
+
+  local dir_name=$dir_name_prefix/dot-scarr
+
+  local dot_pass="dot-cfg"
+  local out_file_suffix="default" # print instructions in dot file
+  if [ $cfg_type == "only" ]; then
+    dot_pass="dot-cfg-only"
+    out_file_suffix="only"
+  fi 
+
+  local llvm_passes=scarr-cp-marker,scarr-loa-collector,$dot_pass
+  if [ "$is_inline" == "inline" ]; then
+    llvm_passs=inliner-wrapper,scarr-cp-marker,scarr-loa-collector,$dot_pass
+    dir_name=$dir_name_prefix/dot-scarr-inline
+  fi
+
+  mkdir -p $dir_name
+  cd $dir_name
+  $LLVM_OPT_CMD ../$title.ll -passes=$llvm_passes > loa.txt
+  for file in $(ls -A); do
+    if [ "${file##*.}" = "dot" ]; then
+      baseFileName="$(basename "$file" .dot)"
+      # Add suffix if it is not added
+      if [[ $baseFileName != *"only"* && $baseFileName != *"default"* ]]; then
+        newName="$baseFileName-$out_file_suffix"
+        mv $file $newName.dot
+        dot -Tpng $newName.dot -o $newName.png
+      fi
+    fi
+  done
+  
+  # Remove all non-main files
+  for file in $(ls -A); do
+  if [[ $file != *"main"* ]]; then
+    rm $file
+  fi
+  
+  done
+  cd $base_dir
+}
+
+dir_name_prefix=$title/no-opt
+if [ -n "$is_optimized" ] && [  "$is_optimized" = "optimized" ]; then
+  dir_name_prefix=$title/opt
+  mkdir -p $dir_name_prefix
+  clang -emit-llvm $title.c -S -o $dir_name_prefix/$title.ll
+
+  generate_dot $dir_name_prefix $title "only" "no-inline" "optimized"
+  generate_dot $dir_name_prefix $title "default" "on-inline" "optimized"
+  generate_dot $dir_name_prefix $title "only" "inline" "optimized"
+  generate_dot $dir_name_prefix $title "default" "inline" "optimized"
+else
+  mkdir -p $dir_name_prefix
+  clang -emit-llvm $title.c -S -Xclang -disable-O0-optnone -fno-discard-value-names -o $dir_name_prefix/$title.ll
+
+  generate_dot $dir_name_prefix $title "only" "no-inline" "no-optimized"
+  generate_dot $dir_name_prefix $title "default" "on-inline" "no-optimized"
+  generate_dot $dir_name_prefix $title "only" "inline" "no-optimized"
+  generate_dot $dir_name_prefix $title "default" "inline" "no-optimized"
+fi
 # Comment attributes line because it prevent the pass to proces the IR
-sed -i 's/^attributes/;&/g' $out_title.ll
-
-mkdir -p $out_title/dot-scarr
-cd $out_title/dot-scarr
-/home/lamida/github/llvm/llvm-project/llvm/cmake-build-release/bin/opt ../../$out_title.ll -passes=scarr-cp-marker,dot-cfg
-ls *.dot | xargs -I{} dot -Tpng {} -o {}.png
-# generate png for hidden files too
-ls .*.dot | xargs -I{} dot -Tpng {} -o {}.png
-cd ../..
-
-
-mkdir -p $out_title/dot-scarr-inline
-cd $out_title/dot-scarr-inline
-/home/lamida/github/llvm/llvm-project/llvm/cmake-build-release/bin/opt ../../$out_title.ll -passes=inliner-wrapper,scarr-cp-marker,dot-cfg
-ls *.dot | xargs -I{} dot -Tpng {} -o {}.png
-# generate png for hidden files too
-ls .*.dot | xargs -I{} dot -Tpng {} -o {}.png
-cd ../..
+sed -i 's/^attributes/;&/g' $dir_name_prefix/$title.ll
